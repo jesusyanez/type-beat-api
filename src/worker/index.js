73d4c86d31e2d0987362019817a93ecda42b8390
jesusyanez <96055925+jesusyanez@users.noreply.github.com>
2022-05-05 08:27:48 -0400

    scraper loop

--- src/worker/index.js
+++ src/worker/index.js
@@ -1,11 +1,19 @@
 import youtube from '@yimura/scraper'
 
-const yt = new youtube.default();
-yt.search('free kodak black type beat').then(results => {
-    for (let i = 0; i < 18; i++) {
-        if (results.videos[i].views > 30000 ) {
-            console.log(results.videos[i]);
-        }
+const artists = ['kendrick', 'jid', 'pooh shiesty', 'j cole', '21 savage']
 
-    }
-});
+
+
+const yt = new youtube.default();
+
+for (let elem in artists) {
+    console.log(artists[elem])
+    yt.search('free '+ artists[elem]+ ' type beat').then(results => {
+        for (let i = 0; i < 18; i++) {
+            if (results.videos[i].views > 30000 ) {
+                console.log(results.videos[i]);
+            }
+    
+        }
+    });
+}
